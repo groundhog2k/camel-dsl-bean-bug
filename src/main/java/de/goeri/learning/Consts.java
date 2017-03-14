dbab7c9e1796e7086b87519ea49379ff343dca1b
Goeran Poehner <goeran.poehner@gmail.com>
2017-03-14 22:11:36 +0100

    Implemented possible workaround

---
+++ src/main/java/de/goeri/learning/Consts.java
@@ -0,0 +1,14 @@
+package de.goeri.learning;
+
+/**
+ * Created by Goeran on 14.03.17.
+ */
+public class Consts {
+    public String getId() { return "ID"; }
+    public String getCountId() { return "COUNT_ID"; }
+    public String getDelta() { return "DELTA"; }
+    public String getTest() { return "TEST"; }
+    public String getStart() { return "START"; }
+    public String getEnd() { return "END"; }
+    public String getLogPoint() { return "LOG_POINT"; }
+}
